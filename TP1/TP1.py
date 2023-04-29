d15de5edf3adbe1d7abfdfa42e4cb58b2d09fdd9
tomasgrau2 <t.grau@alumno.um.edu.ar>
2023-04-29 16:40:16 -0300

    TP1 termniado sin multi

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -7,88 +7,56 @@ def main():
     parser = argparse.ArgumentParser()  
     parser.add_argument('-f','--file',type=str, help='file to be opened')
     args = parser.parse_args()
-
-
-
-    archivo = open(args.file)
+    try:
+        archivo = open(args.file) 
+    except:
+        print("Error: por favor seleccione un archivo válido")
+        return 
     lineas = archivo.readlines()
-
+    numero_linea = 0
+    
     #Cantidad de lineas del archivo
     lineas_count = len(open(args.file).readlines())
 
-  
-    linea_invertida = ""
-
-   
-
     #Creo un tuberia para cada linea
-    pipes = [os.pipe() for _ in range(3)]
-    print(pipes)
+    pipes1 = [os.pipe() for _ in range(lineas_count)]
+    pipes2 = [os.pipe() for _ in range(lineas_count)]
     processes = []
-        
-    numero_linea = 0
-    for i in pipes:
-        # print(i)
-        # print(numero_linea)
+    i2 = 0
+ 
+    for i in pipes1:
         fdr = i[0]
         fdw = i[1]
+        fdr2 = pipes2[i2][0]
+        fdw2 = pipes2[i2][1]
         pid = os.fork()
+
         if pid == 0:
             os.close(fdw)
-            #Leo la linea que envio el padre
+            # Leo la linea que envio el padre, la invierto
             linea = os.read(fdr,100).decode()
-            print(linea)
+            linea = linea[::-1]
+            os.close(fdr)
+            # Envio la linea invertida al padre, usando la segunda tuberia
+            os.write(fdw2, linea.encode())  
+            os.close(fdw2) 
             os._exit(0)
-            
+               
         else:
             os.close(fdr)
             # Envio la linea al hijo
             linea_enviar = lineas[numero_linea]
             numero_linea += 1
             os.write(fdw, linea_enviar.encode())
+
+            # Recibo la linea del hijo
+            linearecibida = os.read(fdr2,100).decode()
+            sys.stdout.write(linearecibida)
+            os.close(fdr2)
             processes.append(pid)
+        i2 += 1 
     for pid in processes:
             os.waitpid(pid,0)
-
-    print(processes)
-    
-        
-
-
-
-
-
-    # if pid == 0:
-    #     os.close(w)
-    #     linea_invertida1 = os.read(r,1000)
-    #     decoded = linea_invertida1.decode()
-    #     linea_invertida = linea_invertida + decoded
-    #     # sys.stdout.write(linea_invertida)
-    #     os.close(rh)
-    #     os.write(wh, linea_invertida.encode())
-
-    # else:
-    #     os.close(r)
-    #     linea = lineas[i]
-    #     linea = linea[::-1]
-    #     os.write(w, linea.encode())
-    #     os.close(wh)
-    #     linea_devuelta = os.read(rh,1000)
-    #     linea_devuelta_decoded = linea_devuelta.decode()
-    #     sys.stdout.write(linea_devuelta_decoded)
-
-
-    # for i in range(3):
-    #     pid = os.fork()
-    
-    #     if pid == 0:
-    #         print("Soy el proceso hijo %d con PID %d" % (i+1, os.getpid()))
-    #     # Código del proceso hijo
-    #         exit(0)
-
-# Código del proceso padre
-    # time.sleep(1)
-    # print("Soy el proceso padre con PID %d" % os.getpid())
 
 if __name__ == '__main__':
     main()
